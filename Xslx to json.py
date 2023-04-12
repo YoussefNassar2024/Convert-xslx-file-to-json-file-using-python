3f60e07d59be3022d86222b182638685be222aac
Youssef Ahmed <94936068+YoussefNassar2024@users.noreply.github.com>
2023-04-12 14:12:52 +0200

    Update Xslx to json.py

--- Xslx to json.py
+++ Xslx to json.py
@@ -1,12 +1,30 @@
 import pandas as pd
 import json
 import os
+##First of all you should drop null rows value##
+################################################
+# Prompt user for file path
+file_path = 'E:\\Your file.xlsx'
 
+# Load Excel file
+df = pd.read_excel(file_path)
+
+# Remove empty rows
+df.dropna(how='all', inplace=True)
+
+# Prompt user for output file path
+output_path = "E:\\cleaned_file.xlsx"
+
+# Save cleaned Excel file
+df.to_excel(output_path, index=False)
+
+##Now choose the cleaned file to convert it to json##
+#####################################################
 # Set the file path
-file_path = "E:\\steps to add Quran Sound\\cleaned_file.xlsx"
+file_path = "E:\\\cleaned_file.xlsx"
 
 # Set the number of rows per split
-rows_per_split = 20
+rows_per_split = 20 #set any number you want
 
 # Load the excel file
 excel_file = pd.ExcelFile(file_path)
@@ -16,10 +34,10 @@ excel_file = pd.ExcelFile(file_path)
 sheet_names = excel_file.sheet_names
 
 # Create the output directory if it does not exist
-output_dir = "E:\\steps to add Quran Sound\\json files\\"
+output_dir = "E:\\json files\\" # files are saved here
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-
+    
 # Split the data into chunks and save each chunk as a json file
 for sheet_name in sheet_names:
     # Load the sheet data into a dataframe
@@ -35,35 +53,3 @@ for sheet_name in sheet_names:
             f.write(chunk.to_json(force_ascii=False, orient='records'))
             print(f"Saved {output_file_path}")
 
-# import pandas as pd
-# import json
-
-# # Define the input and output file paths
-# input_file = 'E:\\steps to add Quran Sound\\cleaned_file.xlsx'
-# output_dir = 'E:\\steps to add Quran Sound\\'
-
-# # Read the Excel file into a Pandas dataframe
-# df = pd.read_excel(input_file)
-
-# # Drop null rows
-# df = df.dropna()
-
-# # Split the dataframe into multiple dataframes based on the number of rows
-# num_rows_per_file = 250
-# num_files = len(df) // num_rows_per_file + 1
-
-# for i in range(num_files):
-#     start_index = i * num_rows_per_file
-#     end_index = min(start_index + num_rows_per_file, len(df))
-#     filename = f'{output_dir}output_{i+1}.json'
-
-#     # Extract the subset of rows from the dataframe
-#     subset_df = df.iloc[start_index:end_index, :]
-
-#     # Convert the subset dataframe to a list of dictionaries and then to JSON
-#     subset_list = subset_df.to_dict('records')
-#     subset_json = json.dumps(subset_list, ensure_ascii=False)
-
-#     # Write the JSON output to a file
-#     with open(filename, 'w', encoding='utf-8') as f:
-#         f.write(subset_json)
